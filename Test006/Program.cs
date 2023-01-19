a95f17375ee918a0274a5d5e47f1516e25d69c32
Elena V <vologdi@yansex.ru>
2023-01-19 18:58:03 +0300

    Test006

---
+++ Test006/Program.cs
@@ -0,0 +1,16 @@
+﻿/*  Напишите программу, которая на вход
+принимает число и выдаёт, является ли число чётным
+(делится ли оно на два без остатка) */
+
+Console.Write("Введите число ");
+int numder = Convert.ToInt32(Console.ReadLine());
+
+if (numder % 2 == 0)
+{
+    Console.WriteLine($"число {numder} является четным");
+}
+else
+{
+    Console.WriteLine($"число {numder} является НЕчетным");
+}
+
