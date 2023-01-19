d5e9f6f983da9f9df96bc85f5bb36944efae3312
Elena V <vologdi@yansex.ru>
2023-01-19 18:58:55 +0300

    Test007

---
+++ Test007/Program.cs
@@ -0,0 +1,12 @@
+﻿/* Напишите программу, которая принимает на вход
+трёхзначное число и на выходе показывает последнюю цифру
+этого числа. */
+
+Console.Write("Введите трехзначное число ");
+int numder = Convert.ToInt32(Console.ReadLine());
+
+int lastDigit = numder % 10;
+
+Console.WriteLine($"цифра {lastDigit} является последней");
+
+
