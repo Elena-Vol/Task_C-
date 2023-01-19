7a7d455d4e664ab185547dee3b0fa14fa4029839
Elena V <vologdi@yansex.ru>
2023-01-19 18:55:23 +0300

    Test004

---
+++ Test004/Program.cs
@@ -0,0 +1,19 @@
+﻿/* Напишите программу, которая принимает на
+вход три числа и выдаёт максимальное из этих чисел */
+
+Console.Write("Введите число ");
+int numderA = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число ");
+int numderB = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число ");
+int numderC = Convert.ToInt32(Console.ReadLine());
+
+int max = numderA;
+
+if (numderB > max) max = numderB;
+if (numderC > max) max = numderC;
+
+Console.Write("max = ");
+Console.WriteLine(max);
