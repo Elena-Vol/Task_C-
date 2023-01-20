f97472159c7b56b1787550f5e4b979189b69ada0
Elena V <vologdi@yansex.ru>
2023-01-20 12:43:08 +0300

    Test022

---
+++ Test022/Program.cs
@@ -0,0 +1,22 @@
+﻿/* Напишите программу, которая
+принимает на вход число (N) и выдаёт таблицу
+квадратов чисел от 1 до N.
+5 -> 1, 4, 9, 16, 25.
+2 -> 1,4 */
+
+Console.WriteLine("введите число ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num < 1) Console.WriteLine("не корректный ввод");
+else Squar (num);
+
+
+void Squar(int number)
+{
+    int count = 1;
+    while (count <= number)
+    {
+        Console.WriteLine($"квадрат числа {count, 5} равен {count * count, 5}");
+        count++;
+    }   
+}
+
