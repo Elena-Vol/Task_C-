eb5b26e48aa3592e353933f16dc29bd9c7f717a4
Elena V <vologdi@yansex.ru>
2023-01-24 17:49:27 +0300

    Test027

---
+++ Test027/Program.cs
@@ -0,0 +1,25 @@
+﻿/* Напишите программу, которая принимает на вход число и 
+выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12 */
+
+Console.Write("Введите целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int module = Math.Abs(number);
+
+int sumDigit = SumDigit(module);
+Console.WriteLine($"Сумма цифр числа {number} = {sumDigit}");
+
+int SumDigit(int num)
+{
+    int sum = 0;
+    while (num > 0)
+    {
+        int digit = num % 10;
+        num = num / 10;
+        sum = sum + digit;
+    }
+    return sum;
+}
