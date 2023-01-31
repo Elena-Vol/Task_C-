0a19fbafb07fef1776ef53e321ce64189e0171ea
Elena V <vologdi@yansex.ru>
2023-01-31 13:07:11 +0300

    Test039_ReverseArray

---
+++ Test039/Program.cs
@@ -0,0 +1,90 @@
+﻿/* Напишите программу, которая перевернёт одномерный массив (последний элемент будет на первом
+месте, а первый - на последнем и т.д.)
+[1 2 3 4 5] -> [5 4 3 2 1]
+[6 7 3 6] -> [6 3 7 6] */
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] array = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(min, max + 1);
+    }
+    return array;
+}
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]}, ");
+        else Console.Write($"{array[i]}");
+    }
+    Console.WriteLine("]");
+}
+
+void ReverseArray (int[] array)
+{
+    for (int i = 0; i < array.Length / 2; i++)
+    {
+        int temp = array[i];
+        array[i] = array[array.Length - 1 - i];
+        array[array.Length -1 - i] = temp;
+    }
+}
+
+
+int[] arr = CreateArrayRndInt(5, 1, 9);
+PrintArray(arr);
+ReverseArray (arr);
+PrintArray(arr);
+Array.Reverse(arr);
+PrintArray(arr);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* void PrintMatrix(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillMatrix(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(1, 10); // интервал от до: [1, 10)
+        }
+    }
+}
+
+int[,] matrix = new int[3, 4];
+PrintMatrix(matrix);
+FillMatrix(matrix);
+Console.WriteLine();
+PrintMatrix(matrix); */
