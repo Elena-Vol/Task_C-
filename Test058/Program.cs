d7fde49dfad4f71174a5e733fccfa496472991f5
Elena V <Elena V>
2023-02-08 18:06:37 +0300

    Test058_Перемножение Матриц

--- Test058/Program.cs
+++ Test058/Program.cs
@@ -7,16 +7,6 @@
 15 18
  */
 
-/*  public static Matrix umn(Matrix a, Matrix b)
-         {
-             Matrix resMass = new Matrix(a.N);
-             for (int i = 0; i < a.N; i++)
-                 for (int j = 0; j < b.N; j++)
-                     for (int k = 0; k < b.N; k++)
-                         resMass[i, j] += a[i, k]*b[k, j];
- 
-             return resMass;
-         } */
 
 /* Матрицу P можно умножить на матрицу K только в том случае, 
 если число столбцов матрицы P равняется числу строк матрицы K. 
@@ -54,24 +44,24 @@ void PrintMatrix(int[,] matrix)
 
 int[,] ProdactMatrix(int[,] MatrixA, int[,] MatrixB)
 {
-    int[,] resMass = new  int [MatrixA.GetLength(0), MatrixB.GetLength(1)];
+    int[,] resMass = new int[MatrixA.GetLength(0), MatrixB.GetLength(1)];
     for (int i = 0; i < MatrixA.GetLength(0); i++)
         for (int j = 0; j < MatrixB.GetLength(1); j++)
-            for (int k = 0; k < MatrixB.GetLength(1); k++)
+            for (int k = 0; k < MatrixA.GetLength(1); k++)
                 resMass[i, j] += MatrixA[i, k] * MatrixB[k, j];
     return resMass;
 }
 
 
-int[,] array2DA = CreateMatrixRndInt(2, 2, 0, 5);
+int[,] array2DA = CreateMatrixRndInt(4, 2, 0, 5);
 PrintMatrix(array2DA);
 Console.WriteLine();
-int[,] array2DB = CreateMatrixRndInt(2, 2, 0, 5);
+int[,] array2DB = CreateMatrixRndInt(2, 3, 0, 5);
 PrintMatrix(array2DB);
 Console.WriteLine();
 if (array2DA.GetLength(1) == array2DB.GetLength(0))
 {
-int[,] prodactMatrix = ProdactMatrix(array2DA, array2DB);
-PrintMatrix(prodactMatrix);
+    int[,] prodactMatrix = ProdactMatrix(array2DA, array2DB);
+    PrintMatrix(prodactMatrix);
 }
 else Console.WriteLine("матрицы перемножить нельзя");
